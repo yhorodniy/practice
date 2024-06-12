41e9c8b43ba7f30af47e8af8feeae42e719c2b7d
Yurii Horodnii <y.horodniy@gmail.com>
2024-06-12 17:53:45 +0300

    file bootstrap & create website

--- helloworld_module/__manifest__.py
+++ helloworld_module/__manifest__.py
@@ -1,29 +1,18 @@
-# {
-#     'name': 'Hello World Module',
-#     'version': '1.0',
-#     'author': 'Your Name',
-#     'depends': ['base', 'website'],
-#     'data': [
-#         # 'security/ir.model.access.csv',
-#         # 'views/hello_world_view.xml',
-#         'data/menu_data.xml',
-#         'data/page_data.xml',
-#         'data/website_data.xml',
-#     ],
-#     'demo': [],
-#     'installable': True,
-#     'application': True,
-# }
-
 {
-    'name': 'Hello World Module',
+    'name': 'Hello World Website',
     'version': '1.0.0',
+    'author': 'Your Name',
+    'description': 'My custom website for Odoo',
     'category': 'Website',
     'depends': ['website'],
     'data': [
+        'views/templates.xml',
+        # 'data/configurator_data.xml',
         'data/website_data.xml',
-        # 'data/page_data.xml',
-        # 'data/menu_data.xml',
+        'data/page_data.xml',
+        'data/menu_data.xml',
     ],
+    'installable': True,
     'application': True,
+    'auto_install': False,
 }
